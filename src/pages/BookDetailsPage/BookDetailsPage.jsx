b023a3353281c8e3052314c8fbcf4a91a902bdbe
Sebatian <Sxe1exs@gmail.com>
2023-12-30 23:11:59 -0700

    Added partial favorite button functionality and styling

--- src/pages/BookDetailsPage/BookDetailsPage.jsx
+++ src/pages/BookDetailsPage/BookDetailsPage.jsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+import { Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
@@ -8,21 +10,45 @@ const BookDetailsPage = ({}) => {
     const { bookId } = useParams();
     const [book, setBook] = useState('');
     const [loading, setLoading] = useState(true);
+    const [notLoggedIn, setNotLoggedIn] = useState(false);
+    const [user] = useAuth();
     console.log(useParams())
-    
+
     const handleSearch = async () => {
-        try {
-          console.log(bookId)
-          const responce = await axios.get(`https://www.googleapis.com/books/v1/volumes/${bookId}`)
-          if (responce.status === 200){
-            console.log(responce.data)
-            setBook(responce.data);
-            setLoading(false)
-          }
-        } catch (error) {
-          console.log("Error searching for bookId:", error)
+      try {
+        console.log(bookId)
+        const responce = await axios.get(`https://www.googleapis.com/books/v1/volumes/${bookId}`)
+        if (responce.status === 200){
+          console.log(responce.data)
+          setBook(responce.data);
           setLoading(false)
         }
+      } catch (error) {
+        console.log("Error searching for bookId:", error)
+        setLoading(false)
+      }
+    }
+
+    const detectUser = () => {
+      if(user){
+        handleFavorite()
+      }
+      else {
+        setNotLoggedIn(true);
+      }
+    }
+
+    const handleFavorite = async () => {
+      const favData = {
+        bookId: bookId,
+        title: book.volumeInfo.title
+      }
+
+      try{
+        await axios.post(`https://localhost:5001/api/favorites`, favData)
+      } catch (error){
+        console.log("Error favoriting book:", error)
+      }
     }
 
     useEffect(() => {
@@ -37,9 +63,15 @@ const BookDetailsPage = ({}) => {
         <div className="book-info">
             <img src={`${book.volumeInfo.imageLinks.thumbnail}`} alt="" />
             <div className="info">
-                <p>{book.volumeInfo.title}</p>
-                <p>{book.volumeInfo.authors}</p>
-                <p>{book.volumeInfo.categories}</p>
+                <div className="info-header">
+                    <h2>{book.volumeInfo.title}</h2>
+                    <p>Author: {book.volumeInfo.authors}</p>
+                    <button onClick={detectUser} className={`favorite`}></button>
+                </div>
+                {notLoggedIn && <Navigate to="/login" />}
+                <hr />
+                <p className="genres">{book.volumeInfo.categories}</p>
+                <p>Description: </p>
                 <p>{book.volumeInfo.description}</p>
             </div>
         </div>
